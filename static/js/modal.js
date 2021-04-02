da9c0bbc85d4e0dedb5711cfd647011bc9a2229c
Bertrand2 <dossogne.bertrand@gmail.com>
2021-04-02 14:07:53 +0200

    admin room page

---
+++ static/js/modal.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const modal = document.querySelector("#modalRoom");
+const showModal = () => {
+    modal.classList.add("active");
+}
+const hideModal = () => {
+    modal.classList.remove("active");
+}
+
+modal.querySelector(".admin-room__modal-background").addEventListener("click", () => {
+    hideModal();
+});
+modal.querySelector("#modalRoomClose").addEventListener("click", () => {
+    hideModal();
+});
+modal.querySelector(".admin-room__modal-panel").addEventListener("click", () => {
+});
+[...document.querySelectorAll(".admin-room__thumbnail-image")].forEach(room => {
+    room.addEventListener("click",() => {
+        showModal();
+    })
+})
