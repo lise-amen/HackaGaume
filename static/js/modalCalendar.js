3695a0fd2ed7beaf6ae62f51f7972652a9d874a3
Bertrand2 <dossogne.bertrand@gmail.com>
2021-04-03 12:17:39 +0200

    admin navbar

--- static/js/modalCalendar.js
+++ static/js/modalCalendar.js
@@ -1,7 +1,30 @@
 "use strict";
 
 const modal = document.querySelector("#modalCalendar");
-const showModal = () => {
+const modalFields = {
+    "room": modal.querySelector("#modalCalendarTitle"),
+    "date": modal.querySelector("#date"),
+    "renter":  modal.querySelector("#renter"),
+    "email":  modal.querySelector("#email"),
+    "phone":  modal.querySelector("#phone"),
+    "peopleAmount":  modal.querySelector("#peopleAmount"),
+}
+const defaultInfo = {
+    "date": "Le − de − à −",
+    "renter": "−",
+    "email": "−",
+    "phone": "−",
+    "peopleAmount": 0
+};
+
+const showModal = (info=defaultInfo) => {
+    console.log(info);
+    modalFields.room.innerText = info.room || "−";
+    modalFields.date.innerText = info.date || "Le − de − à −";
+    modalFields.renter.innerText = info.renter || "−";
+    modalFields.email.innerText = info.email || "−";
+    modalFields.phone.innerText = info.phone || "−";
+    modalFields.peopleAmount.innerText = info.peopleAmount || 0;
     modal.classList.add("active");
 }
 const hideModal = () => {
@@ -16,8 +39,8 @@ modal.querySelector("#modalCalendarClose").addEventListener("click", () => {
 });
 modal.querySelector(".admin__modal-panel").addEventListener("click", () => {
 });
-[...document.querySelectorAll(".fc-daygrid-day-events")].forEach(event => {
-    event.addEventListener("click",() => {
-        showModal();
-    });
-});
+// [...document.querySelectorAll(".fc-daygrid-day-events")].forEach(event => {
+//     event.addEventListener("click",() => {
+//         showModal();
+//     });
+// });
